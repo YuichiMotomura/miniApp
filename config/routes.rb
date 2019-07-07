590ec5acb17af37e08f1505fe25967fcffdfb51e
YuichiMotomura <yu1liveinwellisle@gmail.com>
2019-07-07 15:09:13 +0900

    complete

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
   root  'tweets#index'
+  resources :users, only: [:show]
   resources :tweets
+  
 
 end
